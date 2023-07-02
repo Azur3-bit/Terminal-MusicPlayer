dfb7b0b93ff863a315d504b436bdb922e767db6f
Sunil Kumar Sharma <sharma.sunil12527@gmail.com>
2023-07-02 07:01:06 +0530

    Final Commit - Fixed Remaining Bugs

--- SongDivisonSelector.py
+++ SongDivisonSelector.py
@@ -1,25 +1,17 @@
 # SongDivisonSelector
-
 from serverConnection import establishConnection
 from bs4 import BeautifulSoup
 import requests
-
 print("-- Inside Song Division selector Module -- ")
-
 connection = establishConnection()
 # print(connection.content)
-
 # Creating Soup Object
-
 soup = BeautifulSoup(connection.content, "html5lib")
 # print(soup.prettify())
-
 searchedResult = soup.find_all(class_="main_page_category_music_box")
 # print(searchedResult)
-
 songName_list = []
 links_list = []
-
 for it in searchedResult:
     # print(it)
     bTag = it.find("b")
@@ -29,8 +21,6 @@ for it in searchedResult:
     bTag_stringConversion = bTag_stringConversion[8:-9]
     bTag_stringConversion = bTag_stringConversion.strip()
     songName_list.append(bTag_stringConversion)
-
-
 
 # Links
 for it in searchedResult:
@@ -43,8 +33,6 @@ def SongSelection(user_selectedSong):
     link = links_list[user_selectedSong]
     # print(link)
     return link
-
-# print(songName_list)
 
 
 def showAvailableSongList():
@@ -72,37 +60,4 @@ def showAvailableSongList():
 # Return Song Name 
 def songName():
     return songName_list[userSelection]
-
-# Return Selected Song U
-
-
-# def recursiveCallRequired():
-#     if(song[1:6] == "album"):
-#         return True
-#     else:
-#         return False
-    
-
-# if (song[1:6] == "album"):
-#     selected_songUrl = selectedSong_url_funtion()
-#     # print(url)
-#     # Completing Url
-#     principal_url = "https://pagalnew.com/"
-#     selected_songUrl = principal_url+selected_songUrl
-#     req = requests.get(selected_songUrl)
-#     # print(req.content)
-#     # ------ creating Soup Object ------
-#     soup = BeautifulSoup(req.content, "html5lib")
-#     # print(soup.prettify())
-#     # ------ getting all the Donwloadable Object ------
-#     songs_download = []
-#     songs = soup.find_all(class_="main_page_category_music")
-#     # print(songs)
-
-#     for it in songs:
-#         print("-----")
-#         anchorTag = it.a
-#         hrefElement = anchorTag.get("href")
-#         print(hrefElement)
-#         # songs_download.append(hrefElement)
 
